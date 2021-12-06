50ac90c0446df86b04527642de5b3696f9603e1d
opilane <opilane@HP-CZC6297BB3>
2021-12-06 15:53:43 +0200

    Fixed

--- OdeToFood/Models/RestarauntReview.cs
+++ OdeToFood/Models/RestarauntReview.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Models
 {
-    public class RestarauntReview
+    public class RestarauntReview : IValidatableObject
     {
         public int Id { get; set; }
         [Range(1, 10)]
@@ -19,5 +19,12 @@ namespace OdeToFood.Models
         [DisplayFormat(NullDisplayText = "Anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && Body.Length > 20)
+            {
+                yield return new ValidationResult("Low reviews need longer body");
+            }
+        }
     }
 }
